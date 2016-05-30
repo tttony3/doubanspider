cfb6243516dc8569b64dcf9c5eee55add7bca14a
tttony3 <tonylily11@gmail.com>
2016-05-30 09:20:50 +0800

    获取详情

--- spider.py
+++ spider.py
@@ -1,10 +1,10 @@
 import urllib2
 import re
-# import mysql.connector
+import mysql.connector
 
 
 def gettopbooks(start):
-    # cursor = conn.cursor()
+    cursor = conn.cursor()
     url = 'https://book.douban.com/top250?start=' + str(start)
     pattern = re.compile('<a href.*?subject/(.*?)/".*?\{i:\'(.*?)\'.*?src="(.*?)".*?title="(.*?)".*?class="pl">(.*?)</p>', re.S)
     response = urllib2.urlopen(url)
@@ -12,31 +12,35 @@ def gettopbooks(start):
     items = re.findall(pattern, content)
     for item in items:
         print item[0], start + int(item[1])+1, item[2], item[3], item[4]
-       # cursor.execute('insert into think_topbook (bookid, topnum) values (%s, %s)', [item[0], start +int(item[1])+1])
+        cursor.execute('insert into think_topbook (bookid, topnum,imgsrc,title,description) '
+                       'values (%s, %s, %s, %s, %s)',
+                       [item[0], start +int(item[1])+1, item[2], item[3], item[4]])
 
-    # conn.commit()
-    # cursor.close()
+    conn.commit()
+    cursor.close()
 
 
 def gettopchartbooks():
-    # cursor = conn.cursor()
+    cursor = conn.cursor()
     url = 'https://book.douban.com/chart?subcat=I'
     pattern = re.compile('num-box">(.*?)</.*?subject/(.*?)/">.*?src="(.*?)"/>.*?\d/">(.*?)</a>.*?gray">(.*?)<', re.S)
     response = urllib2.urlopen(url)
     content = response.read().decode('utf-8')
     items = re.findall(pattern, content)
     for item in items:
-        # cursor.execute('insert into think_chartbook (bookid) values (%s)', [item])
-        print item[0], item[1], item[2], item[3], item[4]
-    # conn.commit()
-    # cursor.close()
+        cursor.execute('insert into think_chartbook (chartnum,bookid,imgsrc,title,description) '
+                       'values (%s,%s,%s,%s,%s)',
+                       [item[0], item[1], item[2], item[3].strip(), item[4].strip()])
+        print item[0], item[1], item[2], item[3].strip(), item[4].strip()
+    conn.commit()
+    cursor.close()
 
 
-# conn = mysql.connector.connect(user='root', password='password', database='doubanbook')
-# cursor = conn.cursor()
-# cursor.execute('delete from think_topbook')
-# cursor.execute('delete from think_chartbook')
-# conn.commit()
+conn = mysql.connector.connect(user='root', password='', database='doubanbook')
+cursor = conn.cursor()
+cursor.execute('delete from think_topbook')
+cursor.execute('delete from think_chartbook')
+conn.commit()
 start =0
 print 'gettopbooks'
 while start <= 225:
@@ -44,4 +48,4 @@ while start <= 225:
     start =start+25
 print 'gettopchartbooks'
 gettopchartbooks()
-# conn.close()
+conn.close()
